fbeff5425661f98343ba562169190a51fe03e69e
William Hughes <Bill.Hughes@gcu.edu>
2020-10-13 10:15:20 -0700

    First Commit

---
+++ InClassAssignment4/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace InClassAssignment4
+{
+    partial class InClassAssignment4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtElapsedSeconds = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(242, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of seconds elapsed:";
+            // 
+            // txtElapsedSeconds
+            // 
+            this.txtElapsedSeconds.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtElapsedSeconds.Location = new System.Drawing.Point(304, 18);
+            this.txtElapsedSeconds.Name = "txtElapsedSeconds";
+            this.txtElapsedSeconds.Size = new System.Drawing.Size(100, 24);
+            this.txtElapsedSeconds.TabIndex = 1;
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.ForeColor = System.Drawing.Color.CadetBlue;
+            this.lblOutput.Location = new System.Drawing.Point(56, 58);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 18);
+            this.lblOutput.TabIndex = 2;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConvert.Location = new System.Drawing.Point(214, 140);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(84, 29);
+            this.btnConvert.TabIndex = 3;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // InClassAssignment4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 196);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtElapsedSeconds);
+            this.Controls.Add(this.label1);
+            this.Name = "InClassAssignment4";
+            this.Text = "InClassAssignment4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtElapsedSeconds;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ InClassAssignment4/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Elijah Olmos
+//CST-117
+//October 7, 2020
+//In Class Assignment 4
+
+namespace InClassAssignment4
+{
+    public partial class InClassAssignment4 : Form
+    {
+        public InClassAssignment4()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Calculating days, hours, and minutes from a given amount of seconds
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //define variables
+                int inputSeconds = Convert.ToInt32(txtElapsedSeconds.Text); //store user input
+                var resultNumber = TimeSpan.FromSeconds(inputSeconds);   //prepare a variable to hold our result
+                string resultText = "That is equivalent to ";   //define a template string
+
+                //reset label color from last run
+                lblOutput.ForeColor = System.Drawing.Color.CadetBlue;
+
+                //convert seconds to seconds
+                if ((inputSeconds < 60) & (inputSeconds > 0))
+                {
+                    /*
+                     * general format: set the label text to the template string,
+                     * plus the formatted numeric result, plus the units
+                     */
+                    lblOutput.Text = resultText + inputSeconds + " seconds";
+                }
+                //convert seconds to minutes
+                else if ((inputSeconds >= 60) & (inputSeconds < 3600))
+                {
+                    // %m is a special format that removes leading zeros ( see Microsoft .NET Fundamentals > Custom TimeSpan Format Strings )
+                    lblOutput.Text = resultText + resultNumber.ToString("%m") + " whole minutes";
+                }
+                //convert seconds to hours
+                else if ((inputSeconds >= 3600) & (inputSeconds <= 86400))
+                {
+                    lblOutput.Text = resultText + resultNumber.ToString("%h") + " whole hours";
+                }
+                //convert seconds to days
+                else if (inputSeconds >= 86400)
+                {
+                    lblOutput.Text = resultText + resultNumber.ToString("%d") + " whole days";
+                }
+                else
+                {
+                    //catch-all error block for our if-else chain
+                    lblOutput.ForeColor = System.Drawing.Color.Red;         //change label color
+                    lblOutput.Text = "I am unable to convert that number";  //update label with specific error text
+                }
+            } 
+            catch (FormatException ex)
+            {
+                lblOutput.ForeColor = System.Drawing.Color.Red; //change label color
+                lblOutput.Text = "ERROR: That is not a number"; //update label with specific error text
+            }
+            catch (Exception ex)
+            {
+                lblOutput.ForeColor = System.Drawing.Color.Red; //change label color
+                lblOutput.Text = "ERROR: Something went wrong"; //update label with generic error text
+            }
+        }
+    }
+}
