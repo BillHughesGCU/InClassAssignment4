e6ad54a4aa810f4940518b0648ce35486a1361b4
William Hughes <Bill.Hughes@gcu.edu>
2020-10-16 09:19:14 -0700

    Sync during class2

--- InClassAssignment4/Form1.cs
+++ InClassAssignment4/Form1.cs
@@ -11,7 +11,7 @@ using System.Windows.Forms;
 //Elijah Olmos
 //CST-117
 //October 7, 2020
-//In Class Assignment 4 IN Class Tag test Feature 3 10/16 update
+//In Class Assignment 4 IN Class Tag test Feature 3 10/16 update test
 
 namespace InClassAssignment4
 {
